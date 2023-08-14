7f37096a9d4c66739fec46db297adc2e791b0942
MrDesalernos <maxchanceperez@gmail.com>
2023-08-13 17:22:49 -0700

    Camera follows

--- Top Down RPG/Assets/Scripts/CameraMotor.cs
+++ Top Down RPG/Assets/Scripts/CameraMotor.cs
@@ -12,5 +12,35 @@ public class CameraMotor : MonoBehaviour
     private void LateUpdate()
     {
         Vector3 delta = Vector3.zero;
+
+        //checks if we are inside the bounds on the x axis
+        float deltaX = lookAt.position.x - transform.position.x;
+        if(deltaX > boundX || deltaX < -boundX)
+        {
+            if(transform.position.x < lookAt.position.x)
+            {
+                delta.x = deltaX - boundX;
+            }
+            else
+            {
+                delta.x = deltaX + boundX;
+            }
+        }
+
+        //checks if we are inside the bounds on the y axis
+        float deltaY = lookAt.position.y - transform.position.y;
+        if (deltaY > boundY || deltaY < -boundY)
+        {
+            if (transform.position.y < lookAt.position.y)
+            {
+                delta.y = deltaY - boundY;
+            }
+            else
+            {
+                delta.y = deltaY + boundY;
+            }
+        }
+
+        transform.position += new Vector3(delta.x, delta.y, 0);
     }
 }
